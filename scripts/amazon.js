ee7be8641bed9cffbbd441abb57360705806645c
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-05-27 11:31:52 +0530

    converted the products into the objects in js

---
+++ scripts/amazon.js
@@ -0,0 +1,30 @@
+products = [
+  {
+    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+    price: 10.95,
+    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
+    rating: {
+      stars: 4.5,
+      reviews: 87,
+    },
+  },
+  {
+    name: " Intermediate Size Basketball",
+    price: 20.95,
+    image: "images/products/intermediate-composite-basketball.jpg",
+    rating: {
+      stars: 4.0,
+      reviews: 120,
+    },
+  },
+  {
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    price: 7.99,
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    rating: {
+      stars: 4.0,
+      reviews: 56,
+    },
+  },
+];
+
