1e8df2a65e0aea1bda1832fa700ffc88b065cf7d
Naga sai Satya Teja <nagasai.koli@gmail.com>
2025-05-27 11:57:00 +0530

    modified the the js and amazon.html file to fetch the products form the data given

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,39 +1,3 @@
-products = [
-  {
-    name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
-    price: 10.95,
-    image: "images/products/athletic-cotton-socks-6-pairs.jpg",
-    rating: {
-      stars: 4.5,
-      reviews: 87,
-    },
-    discription:
-      "These athletic cotton socks are designed for comfort and durability, perfect for everyday wear or sports activities. The pack includes six pairs in a stylish black and gray color scheme.",
-  },
-  {
-    name: " Intermediate Size Basketball",
-    price: 20.95,
-    image: "images/products/intermediate-composite-basketball.jpg",
-    rating: {
-      stars: 4.0,
-      reviews: 120,
-    },
-    discription:
-      "This intermediate size basketball is made from high-quality composite leather, providing excellent grip and durability for both indoor and outdoor play. Perfect for players of all skill levels.",
-  },
-  {
-    name: "Adults Plain Cotton T-Shirt - 2 Pack",
-    price: 7.99,
-    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
-    rating: {
-      stars: 4.0,
-      reviews: 56,
-    },
-    discription:
-      "This 2-pack of adults' plain cotton t-shirts offers comfort and style. Made from soft, breathable cotton, these tees are perfect for casual wear or layering. Available in a variety of colors.",
-  },
-];
-
 products.forEach((product) => {
   document.querySelector(
     ".products-grid"
@@ -53,14 +17,14 @@ products.forEach((product) => {
               product.rating.stars * 10
             }.png">
             <div class="product-rating-count link-primary">
-              ${product.rating.reviews} reviews
+              ${product.rating.count} count
             </div>
           </div>
         
-          <div class="product-price">
-            <span class="product-price-amount">$${product.price.toFixed(
-              2
-            )}</span>
+          <div class="product-priceCents">
+            <span class="product-priceCents-amount">$${(
+              product.priceCents / 100
+            ).toFixed(2)}</span>
           </div>
         
           <div class="product-quantity-container">
